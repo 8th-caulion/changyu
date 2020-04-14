198c7263d87311eaaa2fcb218587873d1617f6aa
jjanggyu <lcg6307@naver.com>
2020-04-14 18:01:39 +0900

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+import myapp.views 
+#from .import views
+
+urlpatterns = [
+    path('', myapp.views.home, name="home"),
+    path('profile/', myapp.views.profile, name="profile"),
+]
